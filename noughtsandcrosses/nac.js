81ffe2e785e7fcf4144155bd91dab141ee19fa2a
Billy Everyteenjamespanter <james.panter@nology.io>
2020-02-14 23:43:47 +0000

    changes

--- noughtsandcrosses/nac.js
+++ noughtsandcrosses/nac.js
@@ -1,25 +1,25 @@
-const grid1 = document.getElementById('grid1');
-const grid2 = document.getElementById('grid2');
-const grid3 = document.getElementById('grid3');
-const grid4 = document.getElementById('grid4');
-const grid5 = document.getElementById('grid5');
-const grid6 = document.getElementById('grid6');
-const grid7 = document.getElementById('grid7');
-const grid8 = document.getElementById('grid8');
-const grid9 = document.getElementById('grid9');
-const updatePanel = document.getElementById('update-panel');
+const grid1 = document.getElementById('grid1'),
+    grid2 = document.getElementById('grid2'),
+    grid3 = document.getElementById('grid3'),
+    grid4 = document.getElementById('grid4'),
+    grid5 = document.getElementById('grid5'),
+    grid6 = document.getElementById('grid6'),
+    grid7 = document.getElementById('grid7'),
+    grid8 = document.getElementById('grid8'),
+    grid9 = document.getElementById('grid9'),
+    updatePanel = document.getElementById('update-panel');
 
-const gridSections = [
-    grid1,
-    grid2,
-    grid3,
-    grid4,
-    grid5,
-    grid6,
-    grid7,
-    grid8,
-    grid9
-]
+// const gridSections = [
+//     grid1,
+//     grid2,
+//     grid3,
+//     grid4,
+//     grid5,
+//     grid6,
+//     grid7,
+//     grid8,
+//     grid9
+// ]
 
 const remove = () => {
     grid1.classList.remove('red', 'blue');
@@ -31,42 +31,49 @@ const remove = () => {
     grid7.classList.remove('red', 'blue');
     grid8.classList.remove('red', 'blue');
     grid9.classList.remove('red', 'blue');
-    updatePanel.innerHTML = '';
+    updatePanel.innerHTML = 'Take a turn';
     updatePanel.classList.remove('vibrate-1', 'red', 'blue')
 }
 
 const addTurn = (grid) => {
     const red = document.querySelectorAll('div .red').length;
+    console.log('red ' + red)
     const blue = document.querySelectorAll('div .blue').length;
+    console.log('blue ' + blue)
 
     if (grid.classList.contains('red') || (grid.classList.contains('blue'))) {
         updatePanel.innerHTML = "CAN'T GO THERE";
-
-    } else if (red > blue) {
+    } else if (red >= blue) {
         grid.classList.add('blue')
+        updatePanel.classList.add('red');
+        updatePanel.innerHTML = "RED'S TURN";
     } else {
         grid.classList.add('red')
+        updatePanel.classList.add('blue');
+        updatePanel.innerHTML = "BLUE'S TURN";
     }
-    if (gridSections[0 && 1 && 2].classList.contains('red')) {
+
+    if ((grid1.classList.contains('red') && grid2.classList.contains('red') && grid3.classList.contains('red')) ||
+        (grid4.classList.contains('red') && grid5.classList.contains('red') && grid6.classList.contains('red')) ||
+        (grid7.classList.contains('red') && grid8.classList.contains('red') && grid9.classList.contains('red')) ||
+        (grid1.classList.contains('red') && grid4.classList.contains('red') && grid7.classList.contains('red')) ||
+        (grid2.classList.contains('red') && grid5.classList.contains('red') && grid8.classList.contains('red')) ||
+        (grid3.classList.contains('red') && grid6.classList.contains('red') && grid9.classList.contains('red')) ||
+        (grid1.classList.contains('red') && grid5.classList.contains('red') && grid9.classList.contains('red')) ||
+        (grid3.classList.contains('red') && grid5.classList.contains('red') && grid7.classList.contains('red'))) {
         updatePanel.classList.add('vibrate-1', 'red');
         updatePanel.innerHTML = 'RED WINS';
-
-    } else {}
+    } else if ((grid1.classList.contains('blue') && grid2.classList.contains('blue') && grid3.classList.contains('blue')) ||
+        (grid4.classList.contains('blue') && grid5.classList.contains('blue') && grid6.classList.contains('blue')) ||
+        (grid7.classList.contains('blue') && grid8.classList.contains('blue') && grid9.classList.contains('blue')) ||
+        (grid1.classList.contains('blue') && grid4.classList.contains('blue') && grid7.classList.contains('blue')) ||
+        (grid2.classList.contains('blue') && grid5.classList.contains('blue') && grid8.classList.contains('blue')) ||
+        (grid3.classList.contains('blue') && grid6.classList.contains('blue') && grid9.classList.contains('blue')) ||
+        (grid1.classList.contains('blue') && grid5.classList.contains('blue') && grid9.classList.contains('blue')) ||
+        (grid3.classList.contains('blue') && grid5.classList.contains('blue') && grid7.classList.contains('blue'))) {
+        updatePanel.classList.add('vibrate-1', 'blue');
+        updatePanel.innerHTML = 'BLUE WINS';
+    } else if (red == 5 && blue == 4 || red == 4 && blue == 5) {
+        updatePanel.innerHTML = 'NO WINNER';
+    }
 }
-// // horizontal win conditions
-// grid1, grid2, grid3,
-// grid4, grid5, grid6,
-// grid7, grid8, grid9,
-
-// // vertical win conditions
-// grid1, grid2, grid3
-// grid4, grid5, grid6
-// grid7, grid8, grid9
-
-// // vertical win conditions
-// grid1, grid5, grid9
-// grid3, grid5, grid7
-
-
-
-// (grid1.classList.contains('red') && grid2.classList.contains('red') && grid3.classList.contains('red')) || (grid4.classList.contains('red') && grid5.classList.contains('red') && grid6.classList.contains('red')) || (grid7.classList.contains('red') && grid8.classList.contains('red') && grid9.classList.contains('red')) || (grid1.classList.contains('red') && grid2.classList.contains('red') && grid3.classList.contains('red')) || (grid4.classList.contains('red') && grid5.classList.contains('red') && grid6.classList.contains('red')) || (grid7.classList.contains('red') && grid8.classList.contains('red') && grid9.classList.contains('red')) || (grid1.classList.contains('red') && grid2.classList.contains('red') && grid3.classList.contains('red')) || (grid4.classList.contains('red') && grid5.classList.contains('red') && grid6.classList.contains('red')) || (grid7.classList.contains('red') && grid8.classList.contains('red') && grid9.classList.contains('red'))
