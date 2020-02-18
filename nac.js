8f0c1a214a243c4ed636cd9789cb8373412733f5
James Panter <James.Panter@Nology.local>
2020-02-18 16:31:23 +0000

    Changes

--- nac.js
+++ nac.js
@@ -10,19 +10,11 @@ const grid1 = document.getElementById('grid1'),
     updatePanel = document.getElementById('update-panel'),
     resetButton = document.querySelector('button');
 
-// const gridSections = [
-//     grid1,
-//     grid2,
-//     grid3,
-//     grid4,
-//     grid5,
-//     grid6,
-//     grid7,
-//     grid8,
-//     grid9
-// ]
+const removeUpdatePanelStyling = () => {
+    updatePanel.classList.remove('vibrate-1', 'red', 'blue');
+}
 
-const remove = () => {
+const removeGameGridStyling = () => {
     grid1.classList.remove('red', 'blue');
     grid2.classList.remove('red', 'blue');
     grid3.classList.remove('red', 'blue');
@@ -32,22 +24,23 @@ const remove = () => {
     grid7.classList.remove('red', 'blue');
     grid8.classList.remove('red', 'blue');
     grid9.classList.remove('red', 'blue');
-    updatePanel.innerHTML = 'Blue player starts';
     updatePanel.classList.remove('vibrate-1', 'red', 'blue');
     resetButton.classList.remove('heartbeat');
     updatePanel.classList.add('blue');
     resetButton.style.backgroundColor = 'buttonface';
+    updatePanel.innerHTML = 'Blue player starts';
 }
 
 const addTurn = (grid) => {
     if (!updatePanel.innerHTML.includes('WINS') && !updatePanel.innerHTML.includes('NO WINNER')) {
 
-        updatePanel.classList.remove('red', 'blue');
+        removeUpdatePanelStyling()
         const bluePlayer = document.getElementById("blue-player").value;
         const redPlayer = document.getElementById("red-player").value;
         const red = document.querySelectorAll('#grid .red').length;
         const blue = document.querySelectorAll('#grid .blue').length;
 
+        // prevent going on a grid thats used, add color
         if (grid.classList.contains('red') || (grid.classList.contains('blue'))) {
             updatePanel.innerHTML = "CAN'T GO THERE";
         } else if (red >= blue) {
@@ -59,7 +52,7 @@ const addTurn = (grid) => {
             updatePanel.classList.add('blue');
             updatePanel.innerHTML = `${bluePlayer}'s TURN`;
         }
-
+        // announce no winner
         if (red == 4 && blue == 4) {
             updatePanel.classList.remove('vibrate-1', 'red', 'blue')
             updatePanel.classList.add('vibrate-1');
@@ -67,7 +60,7 @@ const addTurn = (grid) => {
             resetButton.classList.add('heartbeat')
             updatePanel.innerHTML = 'NO WINNER';
         }
-
+        // check win
         if ((grid1.classList.contains('red') && grid2.classList.contains('red') && grid3.classList.contains('red')) ||
             (grid4.classList.contains('red') && grid5.classList.contains('red') && grid6.classList.contains('red')) ||
             (grid7.classList.contains('red') && grid8.classList.contains('red') && grid9.classList.contains('red')) ||
@@ -76,7 +69,7 @@ const addTurn = (grid) => {
             (grid3.classList.contains('red') && grid6.classList.contains('red') && grid9.classList.contains('red')) ||
             (grid1.classList.contains('red') && grid5.classList.contains('red') && grid9.classList.contains('red')) ||
             (grid3.classList.contains('red') && grid5.classList.contains('red') && grid7.classList.contains('red'))) {
-            updatePanel.classList.remove('vibrate-1', 'red', 'blue')
+            removeUpdatePanelStyling();
             updatePanel.classList.add('vibrate-1', 'red');
             updatePanel.innerHTML = `${redPlayer} WINS`;
             resetButton.style.backgroundColor = 'limegreen';
@@ -89,7 +82,7 @@ const addTurn = (grid) => {
             (grid3.classList.contains('blue') && grid6.classList.contains('blue') && grid9.classList.contains('blue')) ||
             (grid1.classList.contains('blue') && grid5.classList.contains('blue') && grid9.classList.contains('blue')) ||
             (grid3.classList.contains('blue') && grid5.classList.contains('blue') && grid7.classList.contains('blue'))) {
-            updatePanel.classList.remove('vibrate-1', 'red', 'blue')
+            removeUpdatePanelStyling();
             updatePanel.classList.add('vibrate-1', 'blue');
             updatePanel.innerHTML = `${bluePlayer} WINS`;
             resetButton.style.backgroundColor = 'limegreen';
